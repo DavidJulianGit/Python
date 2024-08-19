c7ff15e203436f0671b87c31d189bcfd320b69eb
David Schindler <da.schindler@protonmail.com>
2024-08-19 17:12:41 +0200

    Update: Incorporated Mentor Feedback

--- Exercise1.6/recipe_mysql.py
+++ Exercise1.6/recipe_mysql.py
@@ -1,6 +1,5 @@
 import textwrap
 import string
-import mysql.connector
 
 # Constants
 TABLE_NAME = "Recipes"
@@ -36,44 +35,36 @@ cursor.execute(f"""
         {COLUMN_NAME} VARCHAR(50),
         {COLUMN_INGREDIENTS} VARCHAR(255),
         {COLUMN_COOKING_TIME} INT,
-        {COLUMN_DIFFICULTY} VARCHAR(20)
-    )
+        {COLUMN_DIFFICULTY} VARCHAR(20),
+        CHECK({COLUMN_DIFFICULTY} IN ('Easy', 'Medium', 'Intermediate', 'Hard'))
 """)
 
 # print a recipe
-def print_recipe_from_tuple(recipe_tuple, print_id=False):
+def print_recipe_from_tuple(recipe_tuple):
     Id, name, ingredients, cooking_time, difficulty = recipe_tuple
 
     ingredients_str = ""
     for index, ingredient in enumerate(ingredients.split(", "), start=1):
         ingredients_str += f" {index}. {ingredient}\n"
-    
-    if print_id:
-        output = (
-            f"Id: {Id} \n"
-            f"Recipe: {name} \n"
-            f"Ingredients:\n{ingredients_str}"
-            f"Cooking Time (min): {cooking_time}\n"
-            f"Difficulty: {difficulty}\n"
-        )
-    else:
-        output = (
-            f"Recipe: {name} \n"
-            f"Ingredients:\n{ingredients_str}"
-            f"Cooking Time (min): {cooking_time}\n"
-            f"Difficulty: {difficulty}\n"
-        )
+
+    output = (
+        f"Name: {name} \n"
+        f"Ingredients:\n{ingredients_str}"
+        f"Cooking Time (min): {cooking_time}\n"
+        f"Difficulty: {difficulty}\n"
+    )
     print(output)
 
 # print all recipes
-def print_all_recipes(conn, cursor, print_id=False):
-    sql = f"SELECT * FROM {TABLE_NAME}"
+def print_all_recipes(conn, cursor):
+    sql = f"SELECT * FROM {TABLE_NAME};"
     cursor.execute(sql) 
     result = cursor.fetchall()
     print("\nAll recipes:\n------------------------------------------")
 
-    for recipe_tuple in result:
-        print_recipe_from_tuple(recipe_tuple, print_id)
+    for index, recipe_tuple in enumerate(result, start=1):
+        print("-"*5 + f" Recipe #{index} " + "-"*5)
+        print_recipe_from_tuple(recipe_tuple)
 
     print("------------------------------------------")
 
@@ -85,10 +76,12 @@ def create_recipe(conn, cursor):
         if not name:
             raise ValueError("Recipe name must not be empty.")
 
-        cooking_time = int(input("Cooking time (min): ").strip())
-        if cooking_time <= 0:
+        cooking_time_input = input("Cooking time (min): ").strip()
+        if not cooking_time_input.isdigit() or int(cooking_time_input) <= 0  :
             raise ValueError("Cooking time must be a positive integer.")
         
+        cooking_time = int(cooking_time_input)
+
         ingredients_input = input("Ingredients, separated by comma ',': ").strip()
         if not ingredients_input:
             raise ValueError("Ingredients list cannot be empty.")
@@ -99,7 +92,7 @@ def create_recipe(conn, cursor):
         difficulty = calc_difficulty(cooking_time, ingredients)
             
         # 3.3 & 3.4 SQL String and its execution to insert recipe into the db table
-        sql = f"INSERT INTO {TABLE_NAME} ({COLUMN_NAME}, {COLUMN_INGREDIENTS}, {COLUMN_COOKING_TIME}, {COLUMN_DIFFICULTY}) VALUES (%s, %s, %s, %s)"
+        sql = f"INSERT INTO {TABLE_NAME} ({COLUMN_NAME}, {COLUMN_INGREDIENTS}, {COLUMN_COOKING_TIME}, {COLUMN_DIFFICULTY}) VALUES (%s, %s, %s, %s);"
         values = (name, ingredients, cooking_time, difficulty)
     
         cursor.execute(sql, values)
@@ -133,7 +126,7 @@ def search_recipe(conn, cursor):
     all_ingredients = []
     
     # 4.1 SELECTing the ingredients column and saving the data in "results"
-    sql = f"SELECT {COLUMN_INGREDIENTS} FROM {TABLE_NAME}"
+    sql = f"SELECT {COLUMN_INGREDIENTS} FROM {TABLE_NAME};"
     
     try:
         cursor.execute(sql) 
@@ -151,24 +144,29 @@ def search_recipe(conn, cursor):
             if ingredient not in all_ingredients:
                 all_ingredients.append(ingredient)
     
-    all_ingredients = sorted(all_ingredients)
-
-
+    all_ingredients.sort()
 
     # 4.3 Display all ingredients, Store ingredient to be searched for in "search_ingredient"
     print("\nAll ingredients:")
     for index, ingredient in enumerate(all_ingredients, start=1):
         print(f" {index}. {ingredient}")
 
+    # Getting search ingredient
     try:
-        ingredient_num = int(input("\nPlease enter the number of the ingredient you want to search for: ")) - 1
-        search_ingredient = all_ingredients[ingredient_num]
-        print(f"\nAll recipes including '{search_ingredient}'.")
+        ingredient_num = int(input("\nNumber of the ingredient you want to search for: "))
+        
+        if ingredient_num < 1 or ingredient_num > len(all_ingredients):
+            raise IndexError
+        
+        search_ingredient = all_ingredients[ingredient_num-1]
+        print(f"\nAll recipes including '{search_ingredient}'")
+        print(f"-"*len(f"All recipes including '{search_ingredient}'"))
+
     except ValueError:
-        print("Please enter a valid number.")
+        print("Please enter a positive integer.")
         return
     except IndexError:
-        print("Please enter a valid number.")
+        print(f"Please enter the number of an ingredient. (1 - {len(all_ingredients)})")
         return
     except Exception as e:
         print(f"An unexpected error occurred: {e}")   
@@ -176,7 +174,7 @@ def search_recipe(conn, cursor):
 
 
     # 4.4 Searching for rows in Recipe table that contain search_ingredient
-    sql = f"SELECT * FROM {TABLE_NAME} WHERE {COLUMN_INGREDIENTS} LIKE %s"
+    sql = f"SELECT * FROM {TABLE_NAME} WHERE {COLUMN_INGREDIENTS} LIKE %s;"
     
     try:
         cursor.execute(sql, ('%' + search_ingredient + '%',))
@@ -194,28 +192,33 @@ def update_recipe(conn, cursor):
     update_values = ()
     
     # Get all recipes
-    sql = f"SELECT * FROM {TABLE_NAME}"
+    sql = f"SELECT * FROM {TABLE_NAME};"
     cursor.execute(sql)
     result = cursor.fetchall()
 
     # Print all recipes
-    print("\nRecipes.\n--------------------------")
+    print("\nWhich recipe do you want to update?\n--------------------------")
 
-    for recipe_tuple in result:
-        print_recipe_from_tuple(recipe_tuple, True)
+    for index, recipe_tuple in enumerate(result, start=1):
+        print(f"#{index}")
+        print_recipe_from_tuple(recipe_tuple)
    
-    # Get id of the recipe 
-    id_to_update = int(input("Enter 'Id' of the recipe you want to update: "))
-    recipe_to_update = None
-    
-    for recipe in result:
-        if int(recipe[0]) == id_to_update:
-            recipe_to_update = recipe
-            break
-    
-    if recipe_to_update == None:
-        print("No recipe found with that ID.")
+    # Get index of the recipe 
+    try:
+        index_to_update = int(input("Enter number of the recipe you want to update: ")) - 1
+        if index_to_update < 1 or index_to_update > len(result):
+            raise IndexError("No recipe found with that number. Please enter a number from the list above.")
+
+        recipe_to_update = result[index_to_update]
+
+    except ValueError:
+        print("Invalid input. Please enter a valid number corresponding to the recipe.")
         return
+
+    except IndexError as e:
+        print(e)
+        return
+
     
     print(f"Recipe to update: {recipe_to_update[1]}")
 
@@ -224,63 +227,108 @@ def update_recipe(conn, cursor):
     print(" 1 Name")
     print(" 2 Ingredients")
     print(" 3 Cooking Time")
-    column_to_update = int(input("\nPlease enter the number of the category you want to update: "))
 
-    # Update name
-    if column_to_update == 1:
-        new_name = str(input("Please enter the updated name: "))
-        
-        update_sql = f"UPDATE {TABLE_NAME} SET {COLUMN_NAME} = %s WHERE {COLUMN_ID} = %s"
-        update_values = (new_name, id_to_update)
+    try:
+        new_name = ""
+        column_to_update = int(input("\nPlease enter the number of the category you want to update: ").strip())
 
-    # Update ingredients
-    elif column_to_update == 2:
-        new_ingredients_input = str(input("Please enter the updated ingredients. Seperate them by ', ': ").strip())
-        new_ingredients = ", ".join([string.capwords(ingredient.strip()) for ingredient in new_ingredients_input.split(",")])
-        new_difficulty = calc_difficulty(recipe_to_update[3], new_ingredients)
-        
-        update_sql = f"UPDATE {TABLE_NAME} SET {COLUMN_INGREDIENTS} = %s, {COLUMN_DIFFICULTY} = %s WHERE {COLUMN_ID} = %s"
-        update_values = (new_ingredients, new_difficulty, id_to_update)
+        # Update name
+        if column_to_update == 1:
+            new_name = string.capwords(str(input("Please enter the updated name: ")).strip())
 
-    # Update cooking_time
-    elif column_to_update == 3:
-        new_cooking_time = int(input("Please enter the updated cooking time in minutes: "))
-        new_difficulty = calc_difficulty(new_cooking_time, recipe_to_update[2])
-        
-        update_sql = f"UPDATE {TABLE_NAME} SET {COLUMN_COOKING_TIME} = %s, {COLUMN_DIFFICULTY} = %s WHERE {COLUMN_ID} = %s"
-        update_values = (new_cooking_time, new_difficulty, id_to_update)
+            if not new_name:
+                raise ValueError("Name cannot be empty.")
 
-    else:
-        print("Please enter a valid number.")  
-        return   
+            update_sql = f"UPDATE {TABLE_NAME} SET {COLUMN_NAME} = %s WHERE {COLUMN_ID} = %s;"
+            update_values = (new_name, recipe_to_update[0])
+
+        # Update ingredients
+        elif column_to_update == 2:
+            new_ingredients_input = input("Please enter the updated ingredients. Seperate them by ', ': ").strip()
+
+            if not new_ingredients_input:
+                raise ValueError("Ingredients cannot be empty.")
+
+            new_ingredients = ", ".join([string.capwords(ingredient.strip()) for ingredient in new_ingredients_input.split(",")])
+            new_difficulty = calc_difficulty(recipe_to_update[3], new_ingredients)
+            
+            update_sql = f"UPDATE {TABLE_NAME} SET {COLUMN_INGREDIENTS} = %s, {COLUMN_DIFFICULTY} = %s WHERE {COLUMN_ID} = %s;"
+            update_values = (new_ingredients, new_difficulty, recipe_to_update[0])
+
+        # Update cooking_time
+        elif column_to_update == 3:
+            new_cooking_time_input = input("Please enter the updated cooking time in minutes: ")
+
+            if not new_cooking_time_input.isdigit() or int(new_cooking_time_input) <= 0:
+                raise ValueError("Cooking time must be a positive integer.")
+            
+            new_cooking_time = int(new_cooking_time_input)
+            new_difficulty = calc_difficulty(new_cooking_time, recipe_to_update[2])
+            
+            update_sql = f"UPDATE {TABLE_NAME} SET {COLUMN_COOKING_TIME} = %s, {COLUMN_DIFFICULTY} = %s WHERE {COLUMN_ID} = %s;"
+            update_values = (new_cooking_time, new_difficulty, recipe_to_update[0])
+
+        else:
+            print("Please enter a valid number (1, 2 or 3).")  
+            return   
     
+    except ValueError as e:
+        print(f"Input error: {e}")
+        return
+
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}.")
+        return
+
     try:
         cursor.execute(update_sql, update_values)
         conn.commit()
         print("Recipe updated successfully!")
 
-    except ValueError:
-        print("Invalid input. Please enter a valid integer for the 'Id'.")
-
     except Exception as e:
-        print(f"Unexpected error occurred: {e}.")
+        print(f"Database error occurred: {e}.")
             
 # 6 Delete a recipe 
 def delete_recipe(conn, cursor):
-    print_all_recipes(conn, cursor, True)
+    sql = f"SELECT * FROM {TABLE_NAME};"
+    cursor.execute(sql)
+    result = cursor.fetchall()
+
+    # Print all recipes
+    print("\nWhich recipe do you want to delete?\n--------------------------")
+
+    for index, recipe_tuple in enumerate(result, start=1):
+        print(f"#{index}")
+        print_recipe_from_tuple(recipe_tuple)
    
-    id_to_delete = input("Enter 'Id' of the recipe you want to delete: ")
-    sql = f"DELETE FROM {TABLE_NAME} WHERE {COLUMN_ID} = %s"
+    
+    # Get index of the recipe 
+    try:
+        index_to_delete = int(input("Enter number of the recipe you want to delete: ")) 
+        if index_to_delete < 1 or index_to_delete > len(result):
+            raise IndexError("No recipe found with that number. Please enter a number from the list above.")
+
+        recipe_to_delete = result[index_to_delete]
+
+    except ValueError:
+        print("Invalid input. Please enter a valid number corresponding to the recipe.")
+        return
+
+    except IndexError as e:
+        print(e)
+        return
+
+    sql = f"DELETE FROM {TABLE_NAME} WHERE {COLUMN_ID} = %s;"
     
     try:
-        cursor.execute(sql, (id_to_delete,))
+        cursor.execute(sql, (index_to_delete,))
         conn.commit()
 
         # check if the row was deleted from the table 
         if cursor.rowcount > 0:
-            print(f"Recipe with Id {id_to_delete} was successfully deleted.")
+            print(f"Recipe with Id {index_to_delete} was successfully deleted.")
         else:
-            print(f"No recipe found with Id '{id_to_delete}'.")
+            print(f"No recipe found with Id '{index_to_delete}'.")
 
     except ValueError:
         print("Invalid input. Please enter a valid integer for the 'Id'.")
